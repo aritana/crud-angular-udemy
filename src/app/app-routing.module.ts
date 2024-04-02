597fec306130b76b2e1e03e873c52e8d8cffbcbb
noara <tutoravacefet@gmail.com>
2024-04-02 15:10:22 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//components
+import { ListarProdutosComponent } from './components/listar-produtos/listar-produtos.component';
+import { CriarProdutoComponent } from './components/criar-produto/criar-produto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:ListarProdutosComponent},
+  {path:'criar-produto', component:CriarProdutoComponent},
+  {path:'editar-produto/:id', component:CriarProdutoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CriarProdutoComponent } from './components/criar-produto/criar-produto.component';
+import { ListarProdutosComponent } from './components/listar-produtos/listar-produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CriarProdutoComponent,
+    ListarProdutosComponent
   ],
   imports: [
     BrowserModule,
